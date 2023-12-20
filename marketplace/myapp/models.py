d9c20d9d08964147ec77e249a86e302e73e395f3
ShubhenduADutta <shubhendu@flexli.in>
2023-12-20 17:53:23 +0530

    Create a model to store product data and register it to admin

--- marketplace/myapp/models.py
+++ marketplace/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.FloatField()
+    file = models.FileField(upload_to='uploads')
+
+    def __str__(self):
+        return self.name
