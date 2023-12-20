9525c1f2544dc96d71e8fb01a73503dcebce9b0d
ShubhenduADutta <shubhendu@flexli.in>
2023-12-20 19:52:56 +0530

    Add stripe keys to settings

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
+
+STRIPE_SECRET_KEY = os.environ["STRIPE_SECRET_KEY"]
+STRIPE_PUBLIC_KEY = os.environ["STRIPE_PUBLISHABLE_KEY"]
