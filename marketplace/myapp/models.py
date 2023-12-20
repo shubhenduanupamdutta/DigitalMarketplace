14c710d42bd351c3c8bd6d9f8b0850d8ee6b9fbc
ShubhenduADutta <shubhendu@flexli.in>
2023-12-20 20:24:09 +0530

    Order Detail model and migrations

--- marketplace/myapp/models.py
+++ marketplace/myapp/models.py
@@ -11,3 +11,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class OrderDetail(models.Model):
+    customer_email = models.EmailField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    stripe_payment_intent = models.CharField(max_length=255)
+    has_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
