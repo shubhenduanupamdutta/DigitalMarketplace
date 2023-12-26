94be1e795ee29154a3e436f34aa8b54ef2011a2d
ShubhenduADutta <shubhendu@flexli.in>
2023-12-26 21:13:24 +0530

    Add a seller column to product. And ran migrations.

--- marketplace/myapp/models.py
+++ marketplace/myapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.FloatField()
